4672bd46a1599d79bccf7e9117015b46c9c86d73
[Bikram Bk] <bkbikram727@gmail.com>
2025-12-06 16:06:40 +0545

    initial

---
+++ src/main/java/org/example/fluxguard/FluxGuardApplication.java
@@ -0,0 +1,13 @@
+package org.example.fluxguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxGuardApplication.class, args);
+    }
+
+}
