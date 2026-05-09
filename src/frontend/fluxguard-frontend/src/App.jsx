6b5a51c58ce3f40ccae0edd4e93b72b33e72598c
[Bikram Bk] <bkbikram727@gmail.com>
2026-05-09 19:08:53 +0545

    refactor protectedroute

--- src/frontend/fluxguard-frontend/src/App.jsx
+++ src/frontend/fluxguard-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Layout from "./components/Layout";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
